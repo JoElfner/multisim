b8877206363ad71d7157037e62d4a8dc5d4c03fb
JoElfner <joelfner@gmail.com>
2020-09-08 12:59:25 +0200

    retrigger checks

--- multisim/__init__.py
+++ multisim/__init__.py
@@ -5,7 +5,7 @@ from .simenv import SimEnv as Models  # keep backwards compat
 from .simenv import SimEnv  # new API
 from . import all_parts as ap
 from . import precomp_funs as pf
-from . import simenv as se
+from . import simenv as se  # TODO: deprecated?
 from . import std_parts as _sp
 from . import utility_functions as ut
 
