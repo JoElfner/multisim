a8dd11ecaa6ebd110caace4055a3e52c2e2aa089
JElfner <38665102+JoElfner@users.noreply.github.com>
2021-08-26 14:38:48 +0200

    refactoring of utility and precompiled

--- multisim/__init__.py
+++ multisim/__init__.py
@@ -4,9 +4,22 @@ from .version import version as __version__
 from .simenv import SimEnv as Models  # keep backwards compat
 from .simenv import SimEnv  # new API
 from . import all_parts as ap
-from . import precomp_funs as pf
+from ._precompiled import matprops as matprops
+from ._utility import Meters as Meters
+from . import precomp_funs as pf  # TODO: deprecate as soon as refactored
 from . import simenv as se  # TODO: deprecated?
 from . import std_parts as _sp
 from . import utility_functions as ut
 
-__all__ = ['__version__', 'Models', 'SimEnv', 'ap', 'pf', 'se', '_sp', 'ut']
+__all__ = [
+    '__version__',
+    'Models',
+    'SimEnv',
+    'ap',
+    'matprops',
+    'Meters',
+    'pf',
+    'se',
+    '_sp',
+    'ut',
+]
