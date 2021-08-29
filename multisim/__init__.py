105b662da0c046c2f955ee528d3fc1e8b7f1a5e8
JElfner <38665102+JoElfner@users.noreply.github.com>
2021-08-29 15:02:11 +0200

    imports...

--- multisim/__init__.py
+++ multisim/__init__.py
@@ -9,9 +9,9 @@ from . import precomp_funs as pf  # TODO: deprecate as soon as refactored
 from . import simenv as se  # TODO: deprecated?
 from . import std_parts as _sp
 from . import utility_functions as ut  # TODO: deprecate as soon as refactored
-from ._utility import Meters
+# from ._utility import Meters
 # from ._utility import plotting
-from . import _utility.plotting as plotting
+from ._utility import *
 
 # from . import _utility as utility
 
