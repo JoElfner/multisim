fcfc8bd55490ac07c65effc215ba497881fc1c3d
JElfner <38665102+JoElfner@users.noreply.github.com>
2021-08-30 13:00:09 +0200

    Update __init__.py

--- multisim/__init__.py
+++ multisim/__init__.py
@@ -9,6 +9,7 @@ from . import precomp_funs as pf  # TODO: deprecate as soon as refactored
 from . import simenv as se  # TODO: deprecated?
 from . import std_parts as _sp
 from . import utility_functions as ut  # TODO: deprecate as soon as refactored
+
 # from ._utility import Meters
 # from ._utility import plotting
 from ._utility import *
