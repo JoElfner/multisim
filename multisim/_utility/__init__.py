3433254eaa7064f971ea1d4b67644268efd5a627
JElfner <38665102+JoElfner@users.noreply.github.com>
2021-08-30 13:09:39 +0200

    Update __init__.py

--- multisim/_utility/__init__.py
+++ multisim/_utility/__init__.py
@@ -2,5 +2,6 @@ from ..version import version as __version__
 
 from .meters import Meters as Meters
 from . import plotting
+from . import stat_error_measures
 
-__all__ = ['__version__', 'Meters', 'plotting']
+__all__ = ['__version__', 'Meters', 'plotting', 'stat_error_measures']
