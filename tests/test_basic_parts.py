234e21d63ab538274bc53c6d58a275c816b03e91
JElfner <38665102+JoElfner@users.noreply.github.com>
2021-09-01 11:58:12 +0200

    Update test_basic_parts.py

--- tests/test_basic_parts.py
+++ tests/test_basic_parts.py
@@ -30,7 +30,7 @@ def isolation(fn_isolation):
 
 
 # %% test some basic parts
-# test timeframe init
+# test pipe with pump, with and without massflow
 @pytest.mark.parametrize(
     ('theta_init,theta_amb,massflow,expected'),
     [
@@ -112,6 +112,128 @@ def test_pipe_with_pump(make_sim, theta_init, theta_amb, massflow, expected):
     testsim.add_part(
         part=ms.ap.PipeWithPump,
         name='pipe',
+        length=2.0,
+        grid_points=20,
+        start_massflow=massflow,
+        ctrl_required=False,
+        const_val=massflow,
+        **general_specs,
+    )
+
+    testsim.add_open_port('BC_in', constant=True, temperature=85.0)
+    testsim.add_open_port('BC_out', constant=True, temperature=15.0)
+
+    testsim.connect_ports(
+        first_part='BoundaryCondition',
+        first_port='BC_in',
+        scnd_part='pipe',
+        scnd_port='in',
+    )
+    testsim.connect_ports(
+        first_part='BoundaryCondition',
+        first_port='BC_out',
+        scnd_part='pipe',
+        scnd_port='out',
+    )
+
+    testsim.initialize_sim()
+    testsim.start_sim()
+
+    assert np.all(testsim.parts['pipe'].res[-1].round(2) == expected)
+
+
+# test pipe with valve
+@pytest.mark.parametrize(
+    ('theta_init,theta_amb,massflow,expected'),
+    [
+        (
+            60 + 20 * np.sin(np.linspace(0, 2 * np.pi, num=20)),
+            10.0,
+            0.0,
+            np.array(
+                [
+                    60.5,
+                    66.15,
+                    71.88,
+                    76.3,
+                    78.92,
+                    79.46,
+                    77.85,
+                    74.29,
+                    69.14,
+                    62.97,
+                    56.45,
+                    50.28,
+                    45.13,
+                    41.56,
+                    39.96,
+                    40.5,
+                    43.12,
+                    47.54,
+                    53.26,
+                    58.42,
+                ]
+            ),
+        ),
+        (
+            30 + 10 * np.cos(np.linspace(0, 2 * np.pi, num=20)),
+            50.0,
+            0.2,
+            np.array(
+                [
+                    85.0,
+                    85.0,
+                    85.0,
+                    85.0,
+                    84.99,
+                    84.99,
+                    84.99,
+                    84.99,
+                    84.99,
+                    84.99,
+                    84.99,
+                    84.99,
+                    84.98,
+                    84.98,
+                    84.98,
+                    84.98,
+                    84.98,
+                    84.98,
+                    84.98,
+                    84.97,
+                ]
+            ),
+        ),
+    ],
+)
+def _test_pipe_with_valve(make_sim, theta_init, theta_amb, massflow, expected):
+    testsim = make_sim
+
+    # define pipe specifications for all pipes and ports
+    pipe_specs = {'all': {'pipe_type': 'EN10255_medium', 'DN': 'DN25'}}
+    # set general specifications for all parts
+    general_specs = dict(
+        insulation_thickness=1e-2,
+        insulation_lambda=0.035,
+        T_init=theta_init,
+        T_amb=theta_amb,
+        material='carbon_steel',
+        pipe_specs=pipe_specs,
+    )
+
+    testsim.add_part(
+        part=ms.ap.PipeWithPump,
+        name='pipe_wpump',
+        length=2.0,
+        grid_points=20,
+        start_massflow=massflow,
+        ctrl_required=False,
+        const_val=massflow,
+        **general_specs,
+    )
+    testsim.add_part(
+        part=ms.ap.PipeWith3wValve,
+        name='pipe_wvalve',
         length=2.0,
         grid_points=20,
         start_massflow=massflow,
